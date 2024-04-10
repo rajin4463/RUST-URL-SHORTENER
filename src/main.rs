9b9b5e7f9ba2f17af5a870f031ee89af8b3638f4
rajin4463 <rajinsandira@outlook.com>
2024-04-10 19:50:47 +0530

    database created migrations created link redirector half way made

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod routes;
 use std::error::Error;
 use axum::routing::{get, Router};
 use axum_prometheus::PrometheusMetricLayer;
+use dotenvy::dotenv;
+use sqlx::postgres::PgPoolOptions;
 use tower_http::trace::TraceLayer;
 use tracing_subscriber::layer::SubscriberExt;
 use tracing_subscriber::util::SubscriberInitExt;
@@ -9,6 +11,8 @@ use crate::routes::health;
 
 #[tokio::main]
 async fn main() -> Result <(), Box<dyn Error>>{
+    dotenv().ok();
+
     tracing_subscriber::registry()
         .with(
             tracing_subscriber::EnvFilter::try_from_default_env()
@@ -17,14 +21,22 @@ async fn main() -> Result <(), Box<dyn Error>>{
         .with(tracing_subscriber::fmt::layer())
         .init();
 
-    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
+    let db_url = std::env::var("DATABASE_URL")
+        .expect("DATABASE_URL must be set in envirment variable");
 
+    let db = PgPoolOptions::new()
+        .max_connections(20)
+        .connect(&db_url)
+        .await?;
+
+    let (prometheus_layer, metric_handle) = PrometheusMetricLayer::pair();
 
     let app = Router::new()
         .route("/metrics", get(|| async move { metric_handle.render() }))
         .route("/health", get(health))
         .layer(TraceLayer::new_for_http())
-        .layer(prometheus_layer);
+        .layer(prometheus_layer)
+        .with_state(db);
     
     let listner = tokio::net::TcpListener::bind("0.0.0.0:3000")
         .await
--- src/routes.rs
+++ src/routes.rs
@@ -1,6 +1,45 @@
+use axum::body::Body;
+use axum::extract::{Path, State};
 use axum::http::StatusCode;
-use axum::response::IntoResponse;
+use axum::response::{IntoResponse, Response};
+use sqlx::PgPool;
+use crate::utils::internal_error;
+
+#[derive(serde::Deserialize, serde::Serialize)]
+#[serde(rename_all = "camelCase")]
+pub struct Link {
+    pub id: String,
+    pub target_url: String,
+}
 
 pub async fn health() -> impl IntoResponse {
     (StatusCode::OK, "Service is Healthy")
+}
+
+pub async fn redirect(
+    State(pool): State<PgPool>,
+    Path(request_link): Path<String>,
+) -> Result<Response, (StatusCode, String)> {
+    let link = sqlx::query_as!(
+        Link,
+        "select id, target_url from links where id = $1",
+        request_link
+    )
+    .fetch_optional(&pool)
+    .await
+    .map_err(internal_error)?
+    .ok_or_else(|| "Not Found".to_string())
+    .map_err(|err| (StatusCode::NOT_FOUND, err))?;
+
+    tracing::debug!(
+        "Redirecting link id {} to {}",
+        request_link,
+        link.target_url
+    );
+
+    Ok(Response::builder()
+        .status(StatusCode::TEMPORARY_REDIRECT)
+        .header("Location", link.target_url)
+        .body(Body::empty())
+        .expect("This response should always be constructable"))
 }
